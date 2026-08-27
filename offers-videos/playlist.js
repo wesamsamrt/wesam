aa01584ca22f5983b1677a6552931f150820c22b
wesamsamrt <procurement@wesamsa.com>
2026-08-27 15:26:25 +0300

    Update playlist.js

--- offers-videos/playlist.js
+++ offers-videos/playlist.js
@@ -2,8 +2,4 @@
 // سمِّها بالترتيب: 1.mp4 ثم 2.mp4 ثم 3.mp4 وهكذا.
 window.offerVideoCount = 1;
 
-// مقاطع GIF الإضافية تعمل تلقائياً بعد مقاطع MP4 المرقمة.
-window.offerExtraOfferMedia = [
-    "phone-laptop-ad-5s.gif"
-];
 window.offerVideoCount = 2;
