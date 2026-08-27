313ae864413af0776cfa7ea93527ac4168ef8615
wesamsamrt <procurement@wesamsa.com>
2026-08-27 17:18:00 +0300

    Update playlist.js

--- offers-videos/playlist.js
+++ offers-videos/playlist.js
@@ -1,5 +1,5 @@
 // اكتب هنا عدد المقاطع الموجودة في المجلد فقط.
 // سمِّها بالترتيب: 1.mp4 ثم 2.mp4 ثم 3.mp4 وهكذا.
 window.offerVideoCount = 1;
-
 window.offerVideoCount = 2;
+window.offerVideoCount = 3;
